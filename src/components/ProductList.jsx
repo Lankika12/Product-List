7856695d3388271914c41e1615d2fc398508b6af
Lankika <lankika12@hotmail.com>
2023-02-10 02:52:53 +0530

    added missin files

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,122 @@
+import React, { useEffect, useState } from 'react';
+import Products from './Products';
+
+
+
+function ProductList(props) {
+
+    const [productDetails, setProducts] = useState([]);
+    const [search, setSearch] = useState('');
+
+    const [sortType, setSortType] = useState("title");
+
+   
+
+    useEffect(() => {
+        sortArray(sortType);
+    }, [sortType] );
+
+
+    const handleclick = async(e)=> {
+        const deleteditem = e.target.value;
+        console.log(e.target.value)
+        const api = await fetch('https://dummyjson.com/products/'+ deleteditem, {
+        method: 'DELETE',
+        }).then(res => res.json());
+        
+        console.log(api);
+        };
+
+    const sortArray = async (type) => {
+    const api = await fetch('https://dummyjson.com/products').then(res => res.json());
+
+    const types = {
+        category:"category",
+        title:"title",
+        description:"description",
+        price:"price",
+        stock:"stock",
+    };
+
+    const sortProperty = types[type];
+    if (sortProperty==="price" || sortProperty==="stock") {
+        const sorted = [...api.products].sort((a,b) => a[sortProperty] - b[sortProperty]);
+        setProducts(sorted);
+        
+    }else{
+        const sorted = [...api.products].sort((a,b) => {return a[sortProperty].localeCompare(b[sortProperty], 'en' , {sensitivity: 'base'})});
+        setProducts(sorted);
+    }
+ };
+
+
+       
+      return (
+        <div>
+            <div className="product-header">
+                <div className="product-header-inner">
+                    <div className="product-title">
+                    <h1> Pearson Products</h1>
+                    </div>
+                    <div className="search-products">
+                        <div className="search-bar-wrapper">
+                                <input type="search" className="search-bar" placeholder="Search by Product Category....." onChange={(e) =>setSearch(e.target.value)}/>
+                        </div>
+                    </div>
+
+                    <div className="sort-products">
+                        <select defaultValue={'DEFAULT'} onChange={(e) =>setSortType(e.target.value)}>
+                            <option value="DEFAULT" disabled>Sort items</option>
+                            <option value="category">Category</option>
+                            <option value="title">Title</option>
+                            <option value="description">Description</option>
+                            <option value="price">Price</option>
+                            <option value="stock">Stock</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+
+            <div className="product-wrap">
+            {productDetails.filter((productData) => {
+            return search.toString().toLowerCase()===''
+            ? productData
+            : productData.title.toString().toLowerCase().includes(search);
+            }).map(productData => {
+                   return (
+                    <div className='product-wrap-box' key={productData.id}>
+                        <Products
+                        thumbnail={productData.thumbnail}
+                        title={productData.title}
+                        description={productData.description}
+                        category={productData.category} 
+                        price={productData.price}
+                        stock={productData.stock}
+                        >
+                            
+                        </Products>
+                        <button onClick={handleclick} value={productData.id}> Delete</button>
+
+                    </div>
+                   )
+
+                })}
+                
+            </div>
+            
+        </div>
+     )
+    }
+
+
+
+export default ProductList;
+
+
+
+
+
+
+
+
+
---
+++ src/components/Products.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Products(props) {
+  return (
+    <Wrapper key={props.id}>
+        <div className="product-wrapper-inner">            
+            <div className="product-img-wrap">
+            <picture>
+                <img src={props.thumbnail} />
+            </picture>
+            </div>
+            <div className="product-details">
+            
+            <a href="#" alt="">
+                {props.title}
+            </a>
+            <p className="description">{props.description.substring(0, 40)}...</p>
+
+            <span>
+                <p>Category: </p>
+                {props.category.substring(0, 10)}
+            </span>
+
+            <span>
+                <p>Price: </p>
+                {props.price}
+            </span>
+
+            <span>
+                <p>Stock: </p>
+                {props.stock}
+            </span>
+
+            </div>
+           </div>
+         
+      
+    </Wrapper>
+  )
+}
+
+    /*--can creat style component like below --*/
+    const Wrapper = styled.div`
+    min-height: 470px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.15);
+    border-radius: 4px;
+    `;
+
+export default Products
